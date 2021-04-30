8dcfd9c1f333170d3056bceb953712e70f87cab2
avi <hi@avi.im>
2021-04-30 23:13:17 +0530

    use peewee

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import logging
 import re
 from enum import Enum
 from typing import List
+from datetime import datetime
 
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
 from jinja2 import Template
+from peewee import SqliteDatabase, Model, DateTimeField, CharField, FixedCharField, IntegerField, BooleanField
 
 from secrets import TELEGRAM_BOT_TOKEN
 from cowinapi import CoWinAPI
@@ -22,15 +24,38 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-users = {}
-
 CoWinAPIObj = CoWinAPI()
+
+db = SqliteDatabase('users.db')
+
+
+# storage classes
+class User(Model):
+    created_at = DateTimeField(default=datetime.now)
+    updated_at = DateTimeField(default=datetime.now)
+    telegram_id = CharField(max_length=220, unique=True)
+    chat_id = CharField(max_length=220)
+    pincode = FixedCharField(max_length=6, null=True)
+    age_pref = IntegerField(null=True)
+    enabled = BooleanField(default=True)
+
+    class Meta:
+        database = db
 
 
 class AgeRangePref(Enum):
-    Min0 = 0
-    Min18 = 1
-    Min45 = 2
+    Min0 = 1
+    Min18 = 18
+    Min45 = 45
+
+
+def get_enum_by_string(age_pref: str) -> AgeRangePref:
+    if age_pref == "18":
+        return AgeRangePref.Min18
+    elif age_pref == "45":
+        return AgeRangePref.Min45
+    else:
+        return AgeRangePref.Min0
 
 
 def get_main_buttons() -> List[InlineKeyboardButton]:
@@ -45,7 +70,7 @@ def get_age_kb() -> InlineKeyboardMarkup:
         [
             InlineKeyboardButton("18-45", callback_data='age: 18'),
             InlineKeyboardButton("45+", callback_data='age: 45'),
-            InlineKeyboardButton("Both", callback_data='age: 0'),
+            InlineKeyboardButton("Both", callback_data='age: 1'),
         ]
     ]
     return InlineKeyboardMarkup(keyboard)
@@ -88,30 +113,34 @@ def cmd_button_handler(update: Update, ctx: CallbackContext) -> None:
             return
 
 
-def setup_alert_command(update: Update, _: CallbackContext) -> None:
-    global users
-    user_data: dict = users.get(update.effective_user.id, {})
-    if "age_pref" not in user_data:
+def check_if_preferences_are_set(update: Update, _: CallbackContext) -> User:
+    user: User
+    user, _ = User.get_or_create(telegram_id=update.effective_user.id,
+                                 defaults={'chat_id': update.effective_chat.id})
+    if user.age_pref is None:
         update.effective_chat.send_message("select age pref", reply_markup=get_age_kb())
         return
-    if "pincode" not in user_data:
+    if user.pincode is None:
         update.effective_chat.send_message("pincode not setup")
         return
+    return user
 
 
-def check_slots_command(update: Update, _: CallbackContext) -> None:
-    global users
-    user_data: dict = users.get(update.effective_user.id, {})
-    if "age_pref" not in user_data:
-        update.effective_chat.send_message("select age pref", reply_markup=get_age_kb())
-        return
-    if "pincode" not in user_data:
-        update.effective_chat.send_message("pincode not setup")
+def setup_alert_command(update: Update, ctx: CallbackContext) -> None:
+    user = check_if_preferences_are_set(update, ctx)
+    if not user:
         return
 
-    pincode = user_data["pincode"]
+
+def check_slots_command(update: Update, ctx: CallbackContext) -> None:
+    user = check_if_preferences_are_set(update, ctx)
+    if not user:
+        return
+
+    pincode = user.pincode
     vaccination_centers = CoWinAPIObj.calender_by_pin(pincode, CoWinAPI.today())
-    vaccination_centers = [vc for vc in vaccination_centers if vc.has_available_sessions()]
+    if vaccination_centers:
+        vaccination_centers = [vc for vc in vaccination_centers if vc.has_available_sessions()]
     if not vaccination_centers:
         update.effective_chat.send_message("sorry, not slots available")
         return
@@ -139,26 +168,48 @@ def set_age_preference(update: Update, ctx: CallbackContext) -> None:
     query = update.callback_query
     query.answer()
 
-    if age_pref := ctx.match.groupdict().get("age_mg"):
-        user_data: dict = users.get(update.effective_user.id, {})
-        user_data["age_pref"] = age_pref
-        users[update.effective_user.id] = user_data
+    age_pref = ctx.match.groupdict().get("age_mg")
+    if age_pref is None:
+        return
+
+    user: User
+    user, _ = User.get_or_create(telegram_id=update.effective_user.id,
+                                 defaults={'chat_id': update.effective_chat.id})
+    user.age_pref = int(age_pref)
+    user.updated_at = datetime.now()
+    user.save()
+
+    if user.pincode:
+        update.effective_chat.send_message("Age preference has been set",
+                                           reply_markup=InlineKeyboardMarkup([[*get_main_buttons()]]))
+    else:
         update.effective_chat.send_message("Age preference has been set. Please enter your pincode to proceed next")
 
 
 def set_pincode(update: Update, ctx: CallbackContext) -> None:
-    global users
     pincode = ctx.match.groupdict().get("pincode_mg")
     if not pincode:
         return
-    user_data: dict = users.get(update.effective_user.id, {})
-    user_data['pincode'] = pincode
-    users[update.effective_user.id] = user_data
-    update.effective_chat.send_message(F"Pincode is set to {pincode}",
-                                       reply_markup=InlineKeyboardMarkup([[*get_main_buttons()]]))
+    user: User
+    user, _ = User.get_or_create(telegram_id=update.effective_user.id,
+                                 defaults={'chat_id': update.effective_chat.id})
+    user.pincode = pincode
+    user.updated_at = datetime.now()
+    user.save()
+
+    if user.age_pref:
+        update.effective_chat.send_message(F"Pincode is set to {pincode}",
+                                           reply_markup=InlineKeyboardMarkup([[*get_main_buttons()]]))
+    else:
+        update.effective_chat.send_message("Pincode is set. Please set the age preference to proceed next",
+                                           reply_markup=get_age_kb())
 
 
 def main() -> None:
+    # connect and create tables
+    db.connect()
+    db.create_tables([User, ])
+
     updater = Updater(TELEGRAM_BOT_TOKEN)
 
     # Add handlers
